6517dc68ea91f780527fc91177558c5832ed5b37
Alessandro Muiesan <ale@ctu.unimi.it>
2015-07-20 18:04:16 +0200

    very first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name italianismiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the italianismiApp
+ */
+angular.module('italianismiApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name italianismiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the italianismiApp
+ */
+angular.module('italianismiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
