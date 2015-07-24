9c5904590670fa55215abf6606dcae152c1dc167
Alessandro Muiesan <ale@ctu.unimi.it>
2015-07-24 14:41:35 +0200

    aggiunge pagine per suggerimenti e per credits

---
+++ app/scripts/controllers/credits.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name italianismiApp.controller:CreditsCtrl
+ * @description
+ * # CreditsCtrl
+ * Controller of the italianismiApp
+ */
+angular.module('italianismiApp')
+  .controller('CreditsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/form.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name italianismiApp.controller:FormCtrl
+ * @description
+ * # FormCtrl
+ * Controller of the italianismiApp
+ */
+angular.module('italianismiApp')
+  .controller('FormCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
