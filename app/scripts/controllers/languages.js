7dce587f1cb8f3f34ad1fbbd37aa18be585d4734
Alessandro Muiesan <ale@ctu.unimi.it>
2015-07-29 09:39:43 +0200

    sistema inizializzazione mappa

--- app/scripts/controllers/languages.js
+++ app/scripts/controllers/languages.js
@@ -8,26 +8,115 @@
  * Controller of the italianismiApp
  */
 angular.module('italianismiApp')
-  .controller('LanguagesCtrl', function ($scope, $routeParams, engine) {
-  	$scope.languages = engine.languages;
-  	$scope.search = $routeParams.search;
+	.controller('LanguagesCtrl', function ($scope, $routeParams, engine) {
+		$scope.languages = engine.languages;
+		$scope.search = $routeParams.search;
 
-	if ($routeParams.term) {
-		$scope.term = engine.getTerm($routeParams.term);
-	}
-	
-	$scope.hasLanguage = function(language) {
-		if ($scope.term) {
-			var found = false;
-			angular.forEach($scope.term.languages, function(l) {
-				if (l.name === language) {
-					found = true;
+		if ($routeParams.term) {
+			$scope.term = engine.getTerm($routeParams.term);
+		}
+		
+		$scope.hasLanguage = function(language) {
+			if ($scope.term) {
+				var found = false;
+				angular.forEach($scope.term.languages, function(l) {
+					if (l.name === language) {
+						found = true;
+					}
+				});
+				return found;
+			}
+			else {
+				return false;
+			}
+		};
+		
+		var initMap = function() {
+			// http://www.amcharts.com/visited_countries/ qui trovi l'esempio per la selezione dei paesi semplice
+			// http://www.amcharts.com/demos/map-with-curved-lines/
+			var map = AmCharts.makeChart('mapdiv',{
+				type: 'map',
+				theme: 'dark',
+				//pathToImages : "http://cdn.amcharts.com/lib/3/images/",
+				panEventsEnabled : true,
+				backgroundColor : '#fff',
+				backgroundAlpha : 0,
+				dragMap: false,
+				zoomControl: {
+					panControlEnabled : false,
+					zoomControlEnabled : false
+				},
+				dataProvider : {
+					map : 'worldHigh',
+					getAreasFromMap : true,
+					lines: [{
+						latitudes: [45.4636, 50.4422],
+						longitudes: [9.1881, 30.5367]
+					}, {
+						latitudes: [45.4636, 46.9480],
+						longitudes: [9.1881, 7.4481]
+					}, {
+						latitudes: [45.4636, 59.3328],
+						longitudes: [9.1881, 18.0645]
+					}, {
+						latitudes: [45.4636, 40.4167],
+						longitudes: [9.1881, -3.7033]
+					}, {
+						latitudes: [45.4636, 46.0514],
+						longitudes: [9.1881, 14.5060]
+					}, {
+						latitudes: [45.4636, 48.2116],
+						longitudes: [9.1881, 17.1547]
+					}, {
+						latitudes: [45.4636, 44.8048],
+						longitudes: [9.1881, 20.4781]
+					}, {
+						latitudes: [45.4636, 55.7558],
+						longitudes: [9.1881, 37.6176]
+					}, {
+						latitudes: [45.4636, 38.7072],
+						longitudes: [9.1881, -9.1355]
+					}, {
+						latitudes: [45.4636, 54.6896],
+						longitudes: [9.1881, 25.2799]
+					}, {
+						latitudes: [45.4636, 64.1353],
+						longitudes: [9.1881, -21.8952]
+					}, {
+						latitudes: [45.4636, 40.4300],
+						longitudes: [9.1881, -74.0000]
+					}],
+					areas :
+					[
+						{
+							'id': 'IT',
+							'showAsSelected': true
+						},
+						{
+							'id': 'JP',
+							'showAsSelected': true
+						}
+					]
+				},
+				areasSettings : {
+					  autoZoom : false,
+					  color : '#c3c3c2',
+					  colorSolid : '#db1933',
+					  selectedColor : '#db1933',
+					  outlineColor : '#f4f4f3',
+					  rollOverOutlineColor : '#f4f4f3',
+					  balloonText: ''
+				},
+				linesSettings: {
+					arc: -0.7, // this makes lines curved. Use value from -1 to 1
+					arrow: 'end',
+					color: '#CC0000',
+					alpha: 0.4,
+					arrowAlpha: 1,
+					arrowSize: 4
 				}
 			});
-			return found;
-		}
-		else {
-			return false;
-		}
-	};
-  });
+		};
+		
+		setTimeout(initMap, 100);
+	});
