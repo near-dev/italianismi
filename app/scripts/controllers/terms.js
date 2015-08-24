dae3fe8fc096b1bcd7d22a9770bb7876d13f39b0
Alessandro Muiesan <ale@ctu.unimi.it>
2015-08-24 08:48:36 +0200

    sistemata uscita scheda, modificata navigazione, sistemata tastiera virtuale

--- app/scripts/controllers/terms.js
+++ app/scripts/controllers/terms.js
@@ -11,11 +11,12 @@ angular.module('italianismiApp')
   .controller('TermsCtrl', function ($scope, $routeParams, engine) {
 	$scope.language = $routeParams.language;
   	$scope.search = $routeParams.search;
-
   	$scope.terms = engine.getFilteredTerms($scope.language, $scope.search);
 	
 	$scope.startWith = function(actual, expected) {
 		var lowerStr = (actual + "").toLowerCase();
 		return lowerStr.indexOf(expected.toLowerCase()) === 0;
 	};
+	
+	$('#search').bind("keyup", function() { if (!$scope.search) $scope.search = {}; $scope.search.termIta = $('#search').val().toString(); $scope.$digest();  });
   });
